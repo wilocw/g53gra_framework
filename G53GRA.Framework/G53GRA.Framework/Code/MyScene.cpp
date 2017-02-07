14828f9c022f0c815f51ba484d36886814c1acb4
ThomasSmith3 <thomas.smith3@nottingham.ac.uk>
2017-02-07 12:23:41 +0000

    Refactor

--- G53GRA.Framework/G53GRA.Framework/Code/MyScene.cpp
+++ G53GRA.Framework/G53GRA.Framework/Code/MyScene.cpp
@@ -1,23 +1,23 @@
 #include "MyScene.h"
 
-#include "Demos\Triangle.h"
+#include "Triangle.h"
 
 MyScene::MyScene(int argc, char** argv, const char *title, const int& windowWidth, const int& windowHeight)
-	: Scene(argc, argv, title, windowWidth, windowHeight)
-{
-
-}
+	: Scene(argc, argv, title, windowWidth, windowHeight){}
 
 void MyScene::Initialise()
 {
+	// set the background colour of the scene to black
 	glClearColor(static_cast<GLclampf>(0.0f), static_cast<GLclampf>(0.0f), static_cast<GLclampf>(0.0f), static_cast<GLclampf>(1.0f));
-	
+
+	// Create a new triangle object and add it to the list of objects
 	Triangle *t = new Triangle();
 	AddObjectToScene(t);
-
 }
 
+/// set the perspective of camera
 void MyScene::Projection()
 {
-	gluPerspective(static_cast<GLdouble>(60.0), static_cast<GLdouble>(windowWidth) / static_cast<GLdouble>(windowHeight), static_cast<GLdouble>(1.0), static_cast<GLdouble>(1000.0));
+	gluPerspective(static_cast<GLdouble>(60.0), static_cast<GLdouble>(windowWidth) / static_cast<GLdouble>(windowHeight),
+				   static_cast<GLdouble>(1.0), static_cast<GLdouble>(1000.0));
 }
