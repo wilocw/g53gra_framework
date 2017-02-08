a7eecf691bc76e925d7a6299e82f028f0837021e
psxwowa <wcw@ALOE.cs.ad.nottingham.ac.uk>
2017-02-08 16:22:27 +0000

    moved object initialisation from constructor

--- G53GRA.Framework/G53GRA.Framework/Code/MyScene.cpp
+++ G53GRA.Framework/G53GRA.Framework/Code/MyScene.cpp
@@ -6,6 +6,13 @@
 MyScene::MyScene(int argc, char** argv, const char *title, const int& windowWidth, const int& windowHeight)
 	: Scene(argc, argv, title, windowWidth, windowHeight)
 {
+	
+}
+
+void MyScene::Initialise()
+{
+	glClearColor(static_cast<GLclampf>(0.0f), static_cast<GLclampf>(0.0f), static_cast<GLclampf>(0.0f), static_cast<GLclampf>(1.0f));
+
 	// DEMO 3 (Demos/Solar System)
 	// create and add a new triangle to the scene
 	Planet *sun = new Planet(30.0, 0.0f, 12.0f, 0.0f);
@@ -20,12 +27,6 @@ MyScene::MyScene(int argc, char** argv, const char *title, const int& windowWidt
 	AddObjectToScene(sun);
 	AddObjectToScene(mars);
 	AddObjectToScene(earth);
-}
-
-void MyScene::Initialise()
-{
-	glClearColor(static_cast<GLclampf>(0.0f), static_cast<GLclampf>(0.0f), static_cast<GLclampf>(0.0f), static_cast<GLclampf>(1.0f));
-
 }
 
 void MyScene::Projection()
