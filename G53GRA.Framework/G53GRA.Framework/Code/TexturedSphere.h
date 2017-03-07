fb35cae4a3fdc9274374a6a93bfc8beff2593ad0
psxwowa <psxwowa@exmail.nottingham.ac.uk>
2017-03-07 16:42:42 +0000

    commented code

--- G53GRA.Framework/G53GRA.Framework/Code/TexturedSphere.h
+++ G53GRA.Framework/G53GRA.Framework/Code/TexturedSphere.h
@@ -3,17 +3,23 @@
 #include "DisplayableObject.h"
 #include "Animation.h"
 #include "Input.h"
-#include "VectorMath.h"
 
 #include <string>
-
+// Rate of rotation around y (18 degrees per second)
 #define _ROTV 18.0f
+// Pointer index for vertex 0
 #define _VERTEX0 0
+// Pointer index for vertex 1
 #define _VERTEX1 3
+// Pointer index for vertex 2
 #define _VERTEX2 6
+// Pointer index for vertex 3
 #define _VERTEX3 9
+// Pointer index for vertex 4
 #define _VERTEX4 12
+// Pointer index for vertex 5
 #define _VERTEX5 15
+// Cap subdivisons at 7
 #define _MAX_RES 7
 
 class TexturedSphere :
@@ -24,29 +30,32 @@ class TexturedSphere :
 public:
 	TexturedSphere(const std::string& filename);
 	~TexturedSphere();
-
+	
 	void Display();
 	void Update(const double& deltaTime);
 	void HandleKey(unsigned char key, int state, int mx, int my);
 
+	// Manually set resolution
 	inline void SetResolution(int r) { _resolution = r; }
-
+	// Toggle wire frame model
 	inline void ToggleWireFrame() { _flagWireFrame = !_flagWireFrame; }
+	// Toggle rotation animation
 	inline void ToggleAnimation() { _flagAnimation = !_flagAnimation; }
 private:
 	void DrawSphere();
 	void SubDivide(int p_recurse, float *a, float *b, float *c);
 	void DrawFace(float *a, float *b, float *c);
-	
 	void FixSeam(float &a, float &b, float &c);
 
+	// ID to bound texture
 	int _texID;
 	// recursive resolution (increase for finer mesh)
 	int _resolution;
-
+	// Flag for wireframe model
 	GLboolean _flagWireFrame;
+	// Flag for animation
 	GLboolean _flagAnimation;
-
-	float* _INIT_V;
+	// Initial vertices
+	float* _initV;
 };
 
