1e048644a8373c51ee16076ff55c1309c516bcfd
Ward Wil <Wil.Ward@nottingham.ac.uk>
2017-03-10 09:13:35 +0000

    face culling demo

--- G53GRA.Framework/G53GRA.Framework/Code/MyScene.cpp
+++ G53GRA.Framework/G53GRA.Framework/Code/MyScene.cpp
@@ -1,4 +1,5 @@
 #include "MyScene.h"
+#include "FaceCull.h"
 
 MyScene::MyScene(int argc, char** argv, const char *title, const int& windowWidth, const int& windowHeight)
 	: Scene(argc, argv, title, windowWidth, windowHeight)
@@ -8,12 +9,16 @@ MyScene::MyScene(int argc, char** argv, const char *title, const int& windowWidt
 
 void MyScene::Initialise()
 {
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
+	FaceCull *f = new FaceCull();
+	f->size(20.0f);
+	f->position(0.0f, 0.0f, -500.0f);
+	AddObjectToScene(f);
 }
 
 void MyScene::Projection()
 {
 	GLdouble aspect = static_cast<GLdouble>(windowWidth) / static_cast<GLdouble>(windowHeight);
-	gluPerspective(60.0, aspect, 1.0, 1000.0);
+	gluPerspective(60.0, aspect, 1.0, 10000.0);
 }
